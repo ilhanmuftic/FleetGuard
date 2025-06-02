69ccdbb4de5571e57dcf877093a92b2409a170aa
Ilhan Muftic <ilhanmuftic@gmail.com>
2025-06-02 19:08:01 +0200

    rm test supabase

--- client/src/components/AdminDashboard.tsx
+++ client/src/components/AdminDashboard.tsx
@@ -74,7 +74,7 @@ export default function AdminDashboard() {
 
   return (
     <Layout title="Dashboard">
-      <TestSupabaseConnection />
+      {/* <TestSupabaseConnection /> */}
 
       {/* Stats Cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
